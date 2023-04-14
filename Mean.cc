b0c115c05710215cccba78f16d994aa58aea26d9
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-04-14 18:25:31 -0500

    Create Mean.cc

---
+++ Mean.cc
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <iostream>
+#include "Prueba.h"
+
+  JNIEXPORT jdouble JNICALLJava_Prueba_nativeRead(JNIEnv *env, jobject obj, jintArray array) {
+    jint *arr = env->GetIntArrayElements(array, NULL);
+    jsize len = env->GetArrayLength(array);
+
+    std::cout << "Elements of the array: ";
+    for (int i = 0; i < len; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    env->ReleaseIntArrayElements(array, arr, 0);
+}
