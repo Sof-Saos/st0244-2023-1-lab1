79bbbb68fc417893c2f24a1bbcdd670cae3c79cd
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-04-15 17:35:33 -0500

    Update Mean.cc
    We add the code to take the average, remove the test code to read the array and explain how it all works.

--- Mean.cc
+++ Mean.cc
@@ -1,15 +1,17 @@
 #include <jni.h>
-#include <iostream>
 #include "Lab1.h"
 
-  JNIEXPORT jdouble JNICALLJava_Prueba_nativeRead(JNIEnv *env, jobject obj, jintArray array) {
-    jint *arr = env->GetIntArrayElements(array, NULL);
-    jsize len = env->GetArrayLength(array);
+  JNIEXPORT jdouble JNICALLJava_Prueba_nativeRead(JNIEnv *env, jobject obj, jintArray array) { //we define the function that calls the function previously defined in the java program
+    
+    jint *arr = env->GetIntArrayElements(array, NULL); //we get a pointer to the elements of the jintArray object
+    jsize len = env->GetArrayLength(array); //we get the size of the array
 
-    std::cout << "Elements of the array: ";
-    for (int i = 0; i < len; i++) {
-        std::cout << arr[i] << " ";
+    jint counter = 0; //We use a counter that will be used to calculate the sum of the array elements
+    for (int i = 0; i < len; i++) { // for loop that will iterate over each element of the integer array
+        counter += arr[i]; //adds the value of the current array element to the counter variable.
     }
-    std::cout << std::endl;
-    env->ReleaseIntArrayElements(array, arr, 0);
+    jdouble mean = counter/len; //calculate the mean dividing the sum (counter) by the length of the array
+    env->ReleaseIntArrayElements(array, arr, 0); //we release the reference to the jintArray object
+    
+    return mean; //returns the average 
 }
