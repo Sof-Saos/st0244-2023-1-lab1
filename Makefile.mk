dd886eb998ce30b888f014a9898c05245189146c
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-04-15 15:26:49 -0500

    Add files via upload

---
+++ Makefile.mk
@@ -0,0 +1,9 @@
+
+#Esta regla compila el programa
+compile : Lab1.java Mean.cc
+  javac -h . Lab1.java
+  g++ -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -shared -o Lab1.so Mean.cc
+
+#esta regla corre el programa
+run : 
+  java -Djava.library.path=. Lab1 intgs.txt
