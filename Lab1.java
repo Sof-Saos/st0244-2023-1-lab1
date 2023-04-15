31f0332546a4205cc236fe9c656a0288ba5f50df
Sofía Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-04-15 17:04:27 -0500

    Update Lab1.java
    Explanation of each line and elimination of junk code

--- Lab1.java
+++ Lab1.java
@@ -8,42 +8,39 @@ import java.io.IOException;
 
 public class Lab1{
     static {
-        System.loadLibrary("lab1");
+        System.loadLibrary("lab1"); //Load the native library
     }
 
-    private native double nativeRead(int[] array);
+    private native double nativeRead(int[] array); //define the function that will be called
 
     public static void main(String[] args) {
-        ArrayList<Integer> data = new ArrayList<>();
+        ArrayList<Integer> data = new ArrayList<>(); //define an empty array of lists to store the txt data
       
-        try {
+        try { //We use try to handle exceptions that may occur when reading the .txt file.
             FileReader fr = new FileReader(args[0]);
             BufferedReader br = new BufferedReader(fr);
             String line;
-            while ((line = br.readLine()) != null) {
-                String[] partes = line.split(" "); //lista de strings
-                for (String parte : partes) {
-                    int ent = Integer.parseInt(parte);
-                    data.add(ent);
+            while ((line = br.readLine()) != null) { //we read the file line by line
+                String[] partes = line.split(" "); //Separate each line into parts using the space as delimiter.
+                for (String parte : partes) {  //Recorrecting each part of the line
+                    int ent = Integer.parseInt(parte); //We convert the part into an integer
+                    data.add(ent); //add the integer into the arraylist
                 }
             }
-            br.close(); //Cerramos ambos objectos para liberar recursos
+            br.close(); //Close both objects to free resources
             fr.close();
 
-        } catch (IOException e) { //Este catch atrapa los errores que puedan haber y saca un print en caso de no dar
+        } catch (IOException e) { //catches any errors that may occur and outputs a print if it does not give
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
-
-        int[] array = new int[data.size()];
-        for (int i = 0; i < data.size(); i++) {
+        
+        //We convert the arraylist into an array
+        int[] array = new int[data.size()];  //define an empty array with the same size of the arrayList
+        for (int i = 0; i < data.size(); i++) { //copy the elements of the arrayList to the array
             array[i] = data.get(i);
         }
-        IntBuffer intBuffer = ByteBuffer.allocateDirect(array.length * 4).asIntBuffer();
-        //System.out.println("El arreglo en formato arraylist es " + data);
-        intBuffer.put(array);
-        System.out.println("El arreglo es " + intBuffer);
-        //nativeRead();
-        new lab1().readArray(array)
+      
+         new Lab1().nativeRead(array); //we create an instance and call the method with the argument
        
     }
 }
